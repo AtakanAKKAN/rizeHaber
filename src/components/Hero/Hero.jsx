e35bde0f90123298a3342d5be58e7a80d17bda4f
Atakan Akkan <atakan-akkan@hotmail.com>
2024-04-09 11:38:57 +0300

    published

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
           src="https://www.olay53.com/service/webmaster/?t=3&c=10&h=1"
           scrolling="no"
           title="a"
-          frameborder="0"
+          frameBorder="0"
           className="w-full h-[480px] "
         ></iframe>
         <div className="absolute h-[38px] top-0 bg-[#a79277] z-10 max-md: w-full"></div>
@@ -19,7 +19,7 @@ const Hero = () => {
           src="https://www.olay53.com/service/webmaster/?t=2&c=10&h=1"
           scrolling="no"
           title="b"
-          frameborder="0"
+          frameBorder="0"
           className="w-full h-[480px] bg-[#FFF2E1] max-md:h-[300px] "
         ></iframe>
         <div className="absolute w-full h-[38px] top-0 bg-[#a79277] z-10"></div>
